aa584e591d5c1f9a3d7129eb6c357659ab1f5c66
thanhduongnmc24-gif <thanhduongnmc24@gmail.com>
2026-03-02 12:57:38 +0000

    sửa lỗi khong thấy chữ

--- ipa/constants/kho_tap_doc.ts
+++ ipa/constants/kho_tap_doc.ts
@@ -2,6 +2,8 @@
 // Đại ca quăng ảnh vào thư mục assets/images/anh_tap_doc rồi chạy lệnh node để cập nhật nha!
 
 export const TAP_DOC_DATA = [
+  { word: 'bàn chải', image: require('../assets/images/anh_tap_doc/bàn chải.png') },
+  { word: 'chìa khóa', image: require('../assets/images/anh_tap_doc/chìa khóa.png') },
   { word: 'con bò', image: require('../assets/images/anh_tap_doc/con bò.png') },
   { word: 'con chó', image: require('../assets/images/anh_tap_doc/con chó.png') },
   { word: 'con cừu', image: require('../assets/images/anh_tap_doc/con cừu.png') },
@@ -11,4 +13,11 @@ export const TAP_DOC_DATA = [
   { word: 'con mèo', image: require('../assets/images/anh_tap_doc/con mèo.png') },
   { word: 'con trâu', image: require('../assets/images/anh_tap_doc/con trâu.png') },
   { word: 'con vịt', image: require('../assets/images/anh_tap_doc/con vịt.png') },
+  { word: 'la bàn', image: require('../assets/images/anh_tap_doc/la bàn.png') },
+  { word: 'máy ảnh', image: require('../assets/images/anh_tap_doc/máy ảnh.png') },
+  { word: 'nồi đất', image: require('../assets/images/anh_tap_doc/nồi đất.png') },
+  { word: 'sách', image: require('../assets/images/anh_tap_doc/sách.png') },
+  { word: 'điện thoại', image: require('../assets/images/anh_tap_doc/điện thoại.png') },
+  { word: 'đồng hồ', image: require('../assets/images/anh_tap_doc/đồng hồ.png') },
+  { word: 'ấm trà', image: require('../assets/images/anh_tap_doc/ấm trà.png') },
 ];
